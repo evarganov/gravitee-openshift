ce98638171601fda02260e64c8652ee4cd685603
Pavel Klyuev <pavelklyuev@Pavels-MacBook-Pro.local>
2019-01-15 21:47:14 +0300

    update images for minishift

--- openshift/create_all.cmd
+++ openshift/create_all.cmd
@@ -22,18 +22,18 @@ export GRAVITEEIO_VERSION=1.10.4
 # 3. tag this version
 
 # gateway
-oc new-build ../ --name=gateway --context-dir=../images/gateway/ --strategy=docker --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
-oc start-build gateway --from-dir ../images/gateway/ --build-arg GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
+oc new-build ../ --name=gateway --context-dir=images/gateway/ --strategy=docker --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
+oc start-build gateway --from-dir=../images/gateway/ --build-arg GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
 oc tag gateway:latest gateway:$GRAVITEEIO_VERSION
 
 # management-api
 oc new-build ../ --name=management-api --context-dir=images/management-api/ --strategy=docker --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
-oc start-build management-api --from-dir ../images/management-api/ --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
+oc start-build management-api --from-dir=../images/management-api/ --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
 oc tag management-api:latest management-api:$GRAVITEEIO_VERSION
 
 # management-ui
 oc new-build ../ --name=management-ui --context-dir=images/management-ui/ --strategy=docker --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
-oc start-build management-ui --from-dir ../images/management-ui/ --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
+oc start-build management-ui --from-dir=../images/management-ui/ --build-arg=GRAVITEEIO_VERSION=$GRAVITEEIO_VERSION
 oc tag management-ui:latest management-ui:$GRAVITEEIO_VERSION
 
 # import OpenShift Template
